f420f4ca4cc0ee3999bf1005bd36569b6094cc0e
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-09-03 16:30:33 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,22 +1,4 @@
-// const jwt = require("jsonwebtoken");
-
-// const generateAuthTokenandsetCookie = (userId, res) => {
-//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-//     expiresIn: "6d",
-//   });
-//   res.cookie("jwt", token, {
-//     // maxAge:15*24*60*60*1000,//4 days
-//     httpOnly: true,
-//     samsite: "strict",
-//     // secure: process.env.NODE_ENV !== "developement",
-//   });
-// };
-
-// module.exports = generateAuthTokenandsetCookie;
-
-
 const jwt = require("jsonwebtoken");
-
 const generateAuthTokenandsetCookie = (user, res) => {
   const payload = {
     _id: user._id,
@@ -31,10 +13,10 @@ const generateAuthTokenandsetCookie = (user, res) => {
   });
 
   res.cookie("jwt", token, {
-    // maxAge:15*24*60*60*1000, // 4 days
+    maxAge:4*24*60*60*1000, // 4 days
     httpOnly: true,
-    samesite: "strict",
-    // secure: process.env.NODE_ENV !== "development",
+    samesite: "Strict",
+    secure: true,
   });
 };
 
